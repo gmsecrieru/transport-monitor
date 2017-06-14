80c66b401e7aa5b6555acc0b506fa249cddc0fb5
George Secrieru <george.secrieru@gmail.com>
2017-06-14 10:37:03 -0300

    Add AWS/SQS dependencies and configuration

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,7 +1,20 @@
+const SQS_ENDPOINT = 'http://192.168.99.100:9324'
+const SQS_QUEUE_NAME = 'gps_emissions'
+
 export default {
   gateway: {
     url: 'http://192.168.99.100:3000'
   },
   db: 'mongodb://192.168.99.100:27017/transport_monitor',
-  elastic: '192.168.99.100:9200'
+  elastic: '192.168.99.100:9200',
+  aws: {
+    accessKeyId: 'MY_ACCESS_KEY',
+    secretAccessKey: 'MY_SECRET_ACCESS_KEY',
+    sqs: {
+      region: 'us-east-1',
+      endpoint: SQS_ENDPOINT,
+      queueName: SQS_QUEUE_NAME,
+      queueUrl: `${SQS_ENDPOINT}/queue/${SQS_QUEUE_NAME}`
+    }
+  }
 }
